21e983719467a4868799eca950376b2a4a91897c
chankyu <blueriz@naver.com>
2025-01-03 16:12:05 +0900

    Orders_Entity 추가(orders 디렉토리 만들어서 enum class인orders_Status추가)

---
+++ src/main/java/com/aivle/project/entity/orders/OrdersEntity.java
@@ -0,0 +1,55 @@
+package com.aivle.project.entity.orders;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "orders")
+public class OrdersEntity {
+    @Id
+    @Column(nullable = false)
+    private String order_id;
+
+    @PrePersist
+    public void generateId() {
+        if (this.order_id == null) { // ID가 없는 경우만 생성
+            this.order_id = UUID.randomUUID().toString();
+        }
+    }
+
+    private Date order_date;
+
+    private Date sales_date;
+
+    private Float order_amount;
+
+    private Enum<Orders_Status> order_status;
+
+//    @ManyToOne -> entity생성시 해당 코드로 변경
+//    @JoinColumn(name = "contract_id", nullable = false)
+//    private Contract contract;
+    @Column(name = "contract_id", nullable = false)
+    private String contract_id;
+
+//    @ManyToOne
+//    @JoinColumn(name = "product_id", nullable = false)
+//    private Product product;
+    @Column(name = "product_id", nullable = false)
+    private String product_id;
+
+//    @ManyToOne
+//    @JoinColumn(name = "partner_op_id", nullable = false)
+//    private Partner_Op_Month partner_op_month
+    @Column(name = "partner_op_id", nullable = false)
+    private String partner_op_id;
+}
---
+++ src/main/java/com/aivle/project/entity/orders/Orders_Status.java
@@ -0,0 +1,9 @@
+package com.aivle.project.entity.orders;
+
+public enum Orders_Status {
+    none,
+    draft,
+    activated,
+    completed,
+    cancelled
+}
