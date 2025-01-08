4729cca92246820d600684e5159a2b58685bafa6
chankyu <blueriz@naver.com>
2025-01-08 14:22:39 +0900

    불필요한 명령어 삭제

--- src/main/java/com/aivle/project/repository/OrdersRepository.java
+++ src/main/java/com/aivle/project/repository/OrdersRepository.java
@@ -4,11 +4,5 @@ import com.aivle.project.entity.OrdersEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrdersRepository extends JpaRepository<OrdersEntity, Long> {
-//    @Query("SELECT o FROM OrderEntity o WHERE (:order_id IS NULL OR o.order_id = :order_id) " +
-//            "AND (:contract_id IS NULL OR o.contract_id = :contract_id)")
-//    List<OrdersEntity> findByOrderIdOrContractId(
-//                    @Param("order_id") String order_id,
-//                    @Param("contract_id") String contract_id
-//            );
 
 }
