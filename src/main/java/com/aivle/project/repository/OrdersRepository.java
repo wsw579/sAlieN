795edbe22867cee2b94c96f38dd5b657446a01ab
chankyu <blueriz@naver.com>
2025-01-17 20:41:25 +0900

    orders 부분의 데이터 불러오기 방식을 전체에서 한번에 10개씩만 가져오도록 설정

--- src/main/java/com/aivle/project/repository/OrdersRepository.java
+++ src/main/java/com/aivle/project/repository/OrdersRepository.java
@@ -2,10 +2,13 @@ package com.aivle.project.repository;
 
 import com.aivle.project.entity.ContractsEntity;
 import com.aivle.project.entity.OrdersEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface OrdersRepository extends JpaRepository<OrdersEntity, Long> {
     List<OrdersEntity> findByContractId(ContractsEntity contractId);
+    Page<OrdersEntity> findByOrderStatusContainingIgnoreCase(String search, Pageable pageable);
 }
