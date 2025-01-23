a9109c135573a5bfe4806827ca317515379fc3b1
junhyungdev <xk130@naver.com>
2025-01-23 17:46:47 +0900

    Add cors config

--- src/main/java/com/aivle/project/config/SecurityConfig.java
+++ src/main/java/com/aivle/project/config/SecurityConfig.java
@@ -17,6 +17,10 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> cors
+                        .configurationSource(CorsConfig.corsConfigurationSource())
+                );
 
         http
                 .authorizeHttpRequests((auth) -> auth
