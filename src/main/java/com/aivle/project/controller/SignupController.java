7b016ef8f62511360a17c5f1f9491a08785f777d
shiien <ahtlse0917@naver.com>
2025-02-07 15:04:56 +0900

    AC-134 employee 데이터 유효성 검사(클라이언트, 서버)

--- src/main/java/com/aivle/project/controller/SignupController.java
+++ src/main/java/com/aivle/project/controller/SignupController.java
@@ -2,6 +2,7 @@ package com.aivle.project.controller;
 
 import com.aivle.project.dto.EmployeeDto;
 import com.aivle.project.service.SignupService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -35,7 +36,7 @@ public class SignupController {
 
     @PostMapping("/api/signup/register")
     @ResponseBody
-    public ResponseEntity<?> register(@RequestBody EmployeeDto.Post employeeDto) {
+    public ResponseEntity<?> register(@RequestBody @Valid EmployeeDto.Post employeeDto) {
         try {
             signupService.registerUser(employeeDto);
             return ResponseEntity.ok("회원가입이 완료되었습니다.");
